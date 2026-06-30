355989c857ab1f3591195030b7f2738177108066
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-07-01 02:41:34 +0700

    Delete core directory

--- core/save/save_manager.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-const SAVE_DIR := "user://save/"
-const SETTINGS_FILE := "user://save/settings.cfg"
-
-func _ready():
-	if !DirAccess.dir_exists_absolute(SAVE_DIR):
-		DirAccess.make_dir_recursive_absolute(SAVE_DIR) 
--- core/utilities/event_bus.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal settings_changed
-signal scene_loaded(scene_path)
-signal game_paused(paused)
-signal player_spawned(player)
-signal player_died(player) 
